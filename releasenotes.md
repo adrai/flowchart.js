a9ca3c4be024aa589fff3898b952efee28fe4507
Adriano Raiano <adriano@raiano.ch>
2013-07-26 11:33:14 +0200

    fix for old browsers

--- releasenotes.md
+++ releasenotes.md
@@ -1,3 +1,7 @@
+### v1.1.3
+
+- fix for old browsers
+
 ### v1.1.2
 
 - let lines jump
--- src/flowchart.helpers.js
+++ src/flowchart.helpers.js
@@ -10,15 +10,25 @@ function _defaults(options, defaultOptions) {
 }
 
 function _inherits(ctor, superCtor) {
-  ctor.super_ = superCtor;
-  ctor.prototype = Object.create(superCtor.prototype, {
-    constructor: {
-      value: ctor,
-      enumerable: false,
-      writable: true,
-      configurable: true
-    }
-  });
+  if (typeof(Object.create) === 'function') {
+    // implementation from standard node.js 'util' module
+    ctor.super_ = superCtor;
+    ctor.prototype = Object.create(superCtor.prototype, {
+      constructor: {
+        value: ctor,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+  } else {
+    // old school shim for old browsers
+    ctor.super_ = superCtor;
+    var TempCtor = function () {};
+    TempCtor.prototype = superCtor.prototype;
+    ctor.prototype = new TempCtor();
+    ctor.prototype.constructor = ctor;
+  }
 }
 
 // move dependent functions to a container so that
