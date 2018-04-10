d7e1a8abd5ce84297eaf52bc0dba3b306fc012f8
Sudhakar <sudhakar.sekar@itorizon.com>
2018-04-10 23:02:01 +0530

    Flowchart parallel type added

--- src/flowchart.chart.js
+++ src/flowchart.chart.js
@@ -2,151 +2,174 @@ var Raphael = require('raphael');
 var defaults = require('./flowchart.helpers').defaults;
 var defaultOptions = require('./flowchart.defaults');
 var Condition = require('./flowchart.symbol.condition');
+var Parallel = require('./flowchart.symbol.parallel');
 
 function FlowChart(container, options) {
-  options = options || {};
+    options = options || {};
 
-  this.paper = new Raphael(container);
+    this.paper = new Raphael(container);
 
-  this.options = defaults(options, defaultOptions);
+    this.options = defaults(options, defaultOptions);
 
-  this.symbols = [];
-  this.lines = [];
-  this.start = null;
+    this.symbols = [];
+    this.lines = [];
+    this.start = null;
 }
 
 FlowChart.prototype.handle = function(symbol) {
-  if (this.symbols.indexOf(symbol) <= -1) {
-    this.symbols.push(symbol);
-  }
+    if (this.symbols.indexOf(symbol) <= -1) {
+        this.symbols.push(symbol);
+    }
 
-  var flowChart = this;
+    var flowChart = this;
 
-  if (symbol instanceof(Condition)) {
-    symbol.yes = function(nextSymbol) {
-      symbol.yes_symbol = nextSymbol;
-      if(symbol.no_symbol) {
-        symbol.pathOk = true;
-      }
-      return flowChart.handle(nextSymbol);
-    };
-    symbol.no = function(nextSymbol) {
-      symbol.no_symbol = nextSymbol;
-      if(symbol.yes_symbol) {
-        symbol.pathOk = true;
-      }
-      return flowChart.handle(nextSymbol);
-    };
-  } else {
-    symbol.then = function(nextSymbol) {
-      symbol.next = nextSymbol;
-      symbol.pathOk = true;
-      return flowChart.handle(nextSymbol);
-    };
-  }
+    if (symbol instanceof(Condition)) {
+        symbol.yes = function(nextSymbol) {
+            symbol.yes_symbol = nextSymbol;
+            if (symbol.no_symbol) {
+                symbol.pathOk = true;
+            }
+            return flowChart.handle(nextSymbol);
+        };
+        symbol.no = function(nextSymbol) {
+            symbol.no_symbol = nextSymbol;
+            if (symbol.yes_symbol) {
+                symbol.pathOk = true;
+            }
+            return flowChart.handle(nextSymbol);
+        };
+    } else if (symbol instanceof(Parallel)) {
+        symbol.path1 = function(nextSymbol) {
+            symbol.path1_symbol = nextSymbol;
+            if (symbol.path2_symbol) {
+                symbol.pathOk = true;
+            }
+            return flowChart.handle(nextSymbol);
+        };
+        symbol.path2 = function(nextSymbol) {
+            symbol.path2_symbol = nextSymbol;
+            if (symbol.path3_symbol) {
+                symbol.pathOk = true;
+            }
+            return flowChart.handle(nextSymbol);
+        };
+        symbol.path3 = function(nextSymbol) {
+            symbol.path3_symbol = nextSymbol;
+            if (symbol.path1_symbol) {
+                symbol.pathOk = true;
+            }
+            return flowChart.handle(nextSymbol);
+        };
+    } else {
+        symbol.then = function(nextSymbol) {
+            symbol.next = nextSymbol;
+            symbol.pathOk = true;
+            return flowChart.handle(nextSymbol);
+        };
+    }
 
-  return symbol;
+    return symbol;
 };
 
 FlowChart.prototype.startWith = function(symbol) {
-  this.start = symbol;
-  return this.handle(symbol);
+    this.start = symbol;
+    return this.handle(symbol);
 };
 
 FlowChart.prototype.render = function() {
-  var maxWidth = 0,
-      maxHeight = 0,
-      i = 0,
-      len = 0,
-      maxX = 0,
-      maxY = 0,
-      minX = 0,
-      minY = 0,
-      symbol,
-      line;
+    var maxWidth = 0,
+        maxHeight = 0,
+        i = 0,
+        len = 0,
+        maxX = 0,
+        maxY = 0,
+        minX = 0,
+        minY = 0,
+        symbol,
+        line;
 
-  for (i = 0, len = this.symbols.length; i < len; i++) {
-    symbol = this.symbols[i];
-    if (symbol.width > maxWidth) {
-      maxWidth = symbol.width;
+    for (i = 0, len = this.symbols.length; i < len; i++) {
+        symbol = this.symbols[i];
+        if (symbol.width > maxWidth) {
+            maxWidth = symbol.width;
+        }
+        if (symbol.height > maxHeight) {
+            maxHeight = symbol.height;
+        }
     }
-    if (symbol.height > maxHeight) {
-      maxHeight = symbol.height;
+
+    for (i = 0, len = this.symbols.length; i < len; i++) {
+        symbol = this.symbols[i];
+        symbol.shiftX(this.options.x + (maxWidth - symbol.width) / 2 + this.options['line-width']);
+        symbol.shiftY(this.options.y + (maxHeight - symbol.height) / 2 + this.options['line-width']);
     }
-  }
 
-  for (i = 0, len = this.symbols.length; i < len; i++) {
-    symbol = this.symbols[i];
-    symbol.shiftX(this.options.x + (maxWidth - symbol.width)/2 + this.options['line-width']);
-    symbol.shiftY(this.options.y + (maxHeight - symbol.height)/2 + this.options['line-width']);
-  }
+    this.start.render();
+    // for (i = 0, len = this.symbols.length; i < len; i++) {
+    //   symbol = this.symbols[i];
+    //   symbol.render();
+    // }
 
-  this.start.render();
-  // for (i = 0, len = this.symbols.length; i < len; i++) {
-  //   symbol = this.symbols[i];
-  //   symbol.render();
-  // }
-
-  for (i = 0, len = this.symbols.length; i < len; i++) {
-    symbol = this.symbols[i];
-    symbol.renderLines();
-  }
-
-  maxX = this.maxXFromLine;
-
-  var x;
-  var y;
-
-  for (i = 0, len = this.symbols.length; i < len; i++) {
-    symbol = this.symbols[i];
-    x = symbol.getX() + symbol.width;
-    y = symbol.getY() + symbol.height;
-    if (x > maxX) {
-      maxX = x;
+    for (i = 0, len = this.symbols.length; i < len; i++) {
+        symbol = this.symbols[i];
+        symbol.renderLines();
     }
-    if (y > maxY) {
-      maxY = y;
-    }
-  }
 
-  for (i = 0, len = this.lines.length; i < len; i++) {
-    line = this.lines[i].getBBox();
-    x = line.x;
-    y = line.y;
-    var x2 = line.x2;
-    var y2 = line.y2;
-    if (x < minX) {
-      minX = x;
-    }
-    if (y < minY) {
-      minY = y;
-    }
-    if (x2 > maxX) {
-      maxX = x2;
-    }
-    if (y2 > maxY) {
-      maxY = y2;
-    }
-  }
+    maxX = this.maxXFromLine;
 
-  var scale = this.options['scale'];
-  var lineWidth = this.options['line-width'];
+    var x;
+    var y;
 
-  if (minX < 0) minX -= lineWidth;
-  if (minY < 0) minY -= lineWidth;
+    for (i = 0, len = this.symbols.length; i < len; i++) {
+        symbol = this.symbols[i];
+        x = symbol.getX() + symbol.width;
+        y = symbol.getY() + symbol.height;
+        if (x > maxX) {
+            maxX = x;
+        }
+        if (y > maxY) {
+            maxY = y;
+        }
+    }
 
-  var width = maxX + lineWidth - minX;
-  var height = maxY + lineWidth - minY;
+    for (i = 0, len = this.lines.length; i < len; i++) {
+        line = this.lines[i].getBBox();
+        x = line.x;
+        y = line.y;
+        var x2 = line.x2;
+        var y2 = line.y2;
+        if (x < minX) {
+            minX = x;
+        }
+        if (y < minY) {
+            minY = y;
+        }
+        if (x2 > maxX) {
+            maxX = x2;
+        }
+        if (y2 > maxY) {
+            maxY = y2;
+        }
+    }
 
-  this.paper.setSize(width * scale, height * scale);
-  this.paper.setViewBox(minX, minY, width, height, true);
+    var scale = this.options['scale'];
+    var lineWidth = this.options['line-width'];
+
+    if (minX < 0) minX -= lineWidth;
+    if (minY < 0) minY -= lineWidth;
+
+    var width = maxX + lineWidth - minX;
+    var height = maxY + lineWidth - minY;
+
+    this.paper.setSize(width * scale, height * scale);
+    this.paper.setViewBox(minX, minY, width, height, true);
 };
 
 FlowChart.prototype.clean = function() {
-  if (this.paper) {
-    var paperDom = this.paper.canvas;
-    paperDom.parentNode.removeChild(paperDom);
-  }
+    if (this.paper) {
+        var paperDom = this.paper.canvas;
+        paperDom.parentNode.removeChild(paperDom);
+    }
 };
 
 module.exports = FlowChart;
--- src/flowchart.defaults.js
+++ src/flowchart.defaults.js
@@ -1,35 +1,36 @@
 // defaults
 module.exports = {
-  'x': 0,
-  'y': 0,
-  'line-width': 3,
-  'line-length': 50,
-  'text-margin': 10,
-  'font-size': 14,
-  'font-color': 'black',
-  // 'font': 'normal',
-  // 'font-family': 'calibri',
-  // 'font-weight': 'normal',
-  'line-color': 'black',
-  'element-color': 'black',
-  'fill': 'white',
-  'yes-text': 'yes',
-  'no-text': 'no',
-  'arrow-end': 'block',
-  'class': 'flowchart',
-  'scale': 1,
-  'symbols': {
-    'start': {},
-    'end': {},
-    'condition': {},
-    'inputoutput': {},
-    'operation': {},
-    'subroutine': {}
-  }//,
-  // 'flowstate' : {
-  //   'past' : { 'fill': '#CCCCCC', 'font-size': 12},
-  //   'current' : {'fill': 'yellow', 'font-color': 'red', 'font-weight': 'bold'},
-  //   'future' : { 'fill': '#FFFF99'},
-  //   'invalid': {'fill': '#444444'}
-  // }
+    'x': 0,
+    'y': 0,
+    'line-width': 3,
+    'line-length': 50,
+    'text-margin': 10,
+    'font-size': 14,
+    'font-color': 'black',
+    // 'font': 'normal',
+    // 'font-family': 'calibri',
+    // 'font-weight': 'normal',
+    'line-color': 'black',
+    'element-color': 'black',
+    'fill': 'white',
+    'yes-text': 'yes',
+    'no-text': 'no',
+    'arrow-end': 'block',
+    'class': 'flowchart',
+    'scale': 1,
+    'symbols': {
+        'start': {},
+        'end': {},
+        'condition': {},
+        'inputoutput': {},
+        'operation': {},
+        'subroutine': {},
+        'parallel': {}
+    } //,
+    // 'flowstate' : {
+    //   'past' : { 'fill': '#CCCCCC', 'font-size': 12},
+    //   'current' : {'fill': 'yellow', 'font-color': 'red', 'font-weight': 'bold'},
+    //   'future' : { 'fill': '#FFFF99'},
+    //   'invalid': {'fill': '#444444'}
+    // }
 };
